f00b8f915a03733a705c736ee535221de5fdae8c
Basanta Goswami <basantagoswami444@gmail.com>
2024-01-26 14:05:20 +0530

    fix: remove unnecessary import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 // https://vitejs.dev/config/
 export default defineConfig({
