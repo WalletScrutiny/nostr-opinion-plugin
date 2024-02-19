3ab87e80f5f21318576ceb4a6b6907cd741f6746
bit-aloo <sshourya17@gmail.com>
2024-02-19 14:45:37 +0000

    Fix vite proxy script not found

---
+++ index.js
@@ -0,0 +1,8 @@
+import { expertOpinions } from '/src/main.ts';
+expertOpinions.trustedAuthors = [
+    'npub134t5gezg5tl2y2lplnm72f4c06ghnx0leql0t35z8f6hldv9yl6q0lfsfq',
+    'npub1vny7rz3k5pyqxygayx46azxnzh6y6v345jvhzqwcut0l8hyzm4cqv2xl0m'
+];
+expertOpinions.headline = 'Community Opinions ($$nTrusted$$/$$nAll$$)';
+expertOpinions.description =
+    'These comments are contributed by nostr users using the nostr-opinions-plugin.';
