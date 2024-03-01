314f7d5f0e9cd05f8ad034ffaf2c9dd81c364262
Basanta Goswami <basantagoswami444@gmail.com>
2024-03-01 17:35:23 +0530

    Update index.js with comments

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import { expertOpinions } from '/src/main.ts';
 expertOpinions.trustedAuthors = [
-    'npub134t5gezg5tl2y2lplnm72f4c06ghnx0leql0t35z8f6hldv9yl6q0lfsfq',
-    'npub1vny7rz3k5pyqxygayx46azxnzh6y6v345jvhzqwcut0l8hyzm4cqv2xl0m'
+	// John and Jane will be considered trusted users
+	'npub1f70prcng8zwcdjfh5pr7g4txasn40a4pdwyj5vz7x6kwfx4kcrxqn62uvh', // John Nostrich
+	'npub14weyavuc729m2g5jy06ttlvrpn390f25hvsh2nszrvtvd2nuttqs7sjch3' // Jane Nostrich
+];
+expertOpinions.trustedBadges = [
+	// A badge awarded by John, awarded to Jane
+	// 'naddr1qqx4w5ed23jhxapdgfskgem9qyxhwumn8ghj7mn0wvhxcmmvqgsyl8s3uf5r38vxeym6q3ly24nwcf6h76skhzf2xp0rdt8yn2mvpnqrqsqqqafemy2qnm'
+];
+expertOpinions.trustedBadgeAuthors = [
+	// John's npub, all badges by John and the awardees who have accepted those badges will be considered trusted users
+	// 'npub1f70prcng8zwcdjfh5pr7g4txasn40a4pdwyj5vz7x6kwfx4kcrxqn62uvh'
 ];
 expertOpinions.headline = 'Community Opinions ($$nTrusted$$/$$nAll$$)';
 expertOpinions.description =
-    'These comments are contributed by nostr users using the nostr-opinions-plugin.';
+	'These comments are contributed by nostr users using the nostr-opinions-plugin.';
